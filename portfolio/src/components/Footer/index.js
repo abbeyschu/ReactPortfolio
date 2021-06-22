ad8634927aa1448cc6dc9b91693750b2b61b7916
Abbey Schueller <schuellerabbey@gmail.com>
2021-06-21 20:48:32 -0700

    adding photos

---
+++ portfolio/src/components/Footer/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      This is the footer
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ portfolio/src/pages/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+function About() {
+  return (
+    <div>
+        My name is Abbey
+    </div>
+  );
+}
+
+export default About;
