b0ef141cfdcb014340ed52d2ee29678cbc2790a5
Abbey Schueller <schuellerabbey@gmail.com>
2021-06-21 21:12:15 -0700

    adding starting info for content

---
+++ portfolio/src/myinfo.js
@@ -0,0 +1,75 @@
+let myinfo = {
+    "name": "Abbey Schueller",
+    "aboutOne": "Full stack developer.",
+    "aboutTwo": "Currently working in Energy Efficiency",
+    "aboutThree": "Passion for travel.",
+    "location": "Seattle, WA",
+    "imageOne":"./public/images/travel1.jpg",
+    "imageTwo":"./public/images/travel2.jpg",
+    "imageThree":"./public/images/travel3.jpg",
+    "imageFour":"./public/images/travel4.jpg",
+    "educationOne": [
+      {
+        "university": "University of Wisconsin-Madison",
+        "degree": "B.S.,Food Science",
+        "year": "2017",
+      }
+    ],
+    "educationTwo": [
+        {
+          "university": "University of Washington",
+          "degree": "Coding Bootcamp",
+          "year": "2021",
+        }
+      ],
+    "contact":[
+        {
+        "email": "schuellerabbey@gmail.com",
+        "phone": "(262)483-7056"
+        }
+    ],
+    "profileLinks": [
+        {
+        "name": "linkedin",
+        "url": "https://www.linkedin.com/",
+        },
+        {
+        "name": "github",
+        "url": "http://github.com/abbeyschu",
+        }
+      ],
+    "portfolio": [
+        {
+        "title": "title one",
+        "description": "description one",
+        "image": "./public/images/projectOne.png",
+        },
+        {
+        "title": "title two",
+        "description": "description two",
+        "image": "./public/images/projectTwo.png",
+        },
+        {
+        "title": "title three",
+        "description": "description three",
+        "image": "./public/images/projectThree.png",
+        },
+        {
+        "title": "title four",
+        "description": "description four",
+        "image": "./public/images/projectFour.png",
+        },
+        {
+        "title": "title five",
+        "description": "description five",
+        "image": "./public/images/projectFive.png",
+        },
+        {
+        "title": "title six",
+        "description": "description six",
+        "image": "./public/images/projectSix.png",
+        },
+    ],
+
+}
+export default myinfo 
